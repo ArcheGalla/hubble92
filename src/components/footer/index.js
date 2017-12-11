d31ac8894f57451569b2759a66deeea8709ed41a
Artem Pushkar <artem.pushkar@globallogic.com>
2017-12-11 14:31:59 +0200

    - update project move it to preact cli app

---
+++ src/components/footer/index.js
@@ -0,0 +1,13 @@
+import { h, Component } from 'preact';
+//import { Link } from 'preact-router/match';
+import style from './footer.css';
+
+export default class Footer extends Component {
+		render() {
+				return (
+					<footer class={style.footer}>
+
+					</footer>
+				);
+		}
+}
